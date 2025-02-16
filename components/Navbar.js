28a2da2124c313c443b32f4865d661c474347156
it21096884 <it21096884@my.sliit.lk>
2025-02-16 20:46:40 +0530

    fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
 
   return (
     
-      <nav className="fixed top-0 left-0 right-0 z-50 bg-gray-200 dark:bg-gray-900">
+      <nav className={`${darkMode ? "bg-gray-900" : "bg-gray-200"} fixed top-0 left-0 right-0 z-50`}>
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
         <h1 className="text-xl font-bold text-gray-900 dark:text-white">Oshadi Senevirathna</h1>
         
